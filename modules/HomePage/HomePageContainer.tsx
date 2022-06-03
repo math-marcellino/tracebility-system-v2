ef04cf93ef4ce319920e8cd7cae5c7a0d9c84511
William <79161142+Wil-lab07@users.noreply.github.com>
2022-06-03 19:30:24 +0700

    initial commit

--- modules/HomePage/HomePageContainer.tsx
+++ modules/HomePage/HomePageContainer.tsx
@@ -1,4 +1,4 @@
-import type { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import RPHTable from './components/RPHTable';
 import DistributorTable from './components/DistributorTable';
 import HotelTable from './components/HotelTable';
@@ -6,11 +6,20 @@ import HotelTable from './components/HotelTable';
 type HomePageContainerProps = {};
 
 const HomePageContainer: FunctionComponent<HomePageContainerProps> = ({}) => {
+    const [table, setTable] = useState("RPH");
+    console.log(table)
     return (
-        <div>
-            {/* <RPHTable/> */}
-            {/* <DistributorTable/> */}
-            <HotelTable/>
+        <div className="space-y-3">
+            <div className="px-16 space-x-4">
+                <button className= {`border rounded-md w-24 py-1 ${table === "RPH" && "bg-white text-gray-900"}`} onClick={()=>{setTable("RPH")}}>RPH</button>
+                <button className={`border rounded-md w-24 py-1 ${table === "Distributor" && "bg-white text-gray-900"}`} onClick={()=>{setTable("Distributor")}}>Distributor</button> 
+                <button className={`border rounded-md w-24 py-1 ${table === "Hotel" && "bg-white text-gray-900"}`} onClick={()=>{setTable("Hotel")}}>Hotel</button>
+            </div>
+            <div className="">
+                {table === "RPH" && <RPHTable/>}
+                {table === "Distributor" && <DistributorTable/>}
+                {table === "Hotel" && <HotelTable/>}
+            </div>
         </div>
     );
 };
