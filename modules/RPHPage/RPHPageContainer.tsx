3fabc3d5ad14434a1d6473bb4218ae46737b3d5e
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-06-03 15:25:09 +0700

    fix: RPH page updated

--- modules/RPHPage/RPHPageContainer.tsx
+++ modules/RPHPage/RPHPageContainer.tsx
@@ -1,60 +1,63 @@
 import type { FunctionComponent } from 'react';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-import { useProvider } from 'wagmi'
-import { jsonABI, contractAddress, privateKey } from '../../ABI/contractABI'
+import { useState } from 'react';
+import { useProvider } from 'wagmi';
+import { useUserContext } from '../UserContext';
+import { jsonABI, contractAddress, privateKey } from '../../ABI/contractABI';
 import { ethers } from 'ethers';
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 type RPHPageContainerProps = {};
 
 const RPHPageContainer: FunctionComponent<RPHPageContainerProps> = ({}) => {
-    const [rphInfo, setRphInfo] = useState({})
+    const [rphInfo, setRphInfo] = useState({});
     const [isLoading, setIsLoading] = useState(false);
-    
-    const provider = useProvider()
-    const walletSigner = new ethers.Wallet(privateKey, provider)
-    const useContract = new ethers.Contract(contractAddress, jsonABI, walletSigner)
+    const { namaLengkap } = useUserContext();
+
+    const provider = useProvider();
+    const walletSigner = new ethers.Wallet(privateKey, provider);
+    const ContractInstance = new ethers.Contract(
+        contractAddress,
+        jsonABI,
+        walletSigner
+    );
 
     const handleInput = async () => {
-        setIsLoading(true)
+        setIsLoading(true);
         try {
             const {
-                jenisKelamin, 
-                tanggalPemotongan, 
-                statusPemotongan, 
-                sertifHalal
-            }: any = rphInfo
-            const tx = await useContract.createItem(
-                jenisKelamin, 
-                tanggalPemotongan, 
-                statusPemotongan, 
+                jenisKelamin,
+                tanggalPemotongan,
+                statusPemotongan,
                 sertifHalal,
-                "William Chandra"
-            )
-            console.log(tx)
-            await tx.wait()
-            setIsLoading(false)
-            toast.success(`Transaction is sucessfully submitted!`)
-        } catch(err) {
-            setIsLoading(false)
+            }: any = rphInfo;
+            const tx = await ContractInstance.createItem(
+                jenisKelamin,
+                tanggalPemotongan,
+                statusPemotongan,
+                sertifHalal,
+                namaLengkap
+            );
+            console.log(tx);
+            await tx.wait();
+            setIsLoading(false);
+            toast.success(`Transaction is sucessfully submitted!`);
+        } catch (err) {
+            setIsLoading(false);
             console.log(err);
-            toast.error(`${err}`)
+            toast.error(`${err}`);
         }
     };
-    return( 
+    return (
         <div className="flex h-screen w-screen items-center justify-center">
             <div className="bg-gray-700 flex flex-col items-center justify-center px-8 py-6 rounded-xl shadow-xl text-lg gap-6 min-w-[450px]">
-                <p className="text-3xl font-bold">
-                    Input Data RPH
-                </p>
+                <p className="text-3xl font-bold">Input Data RPH</p>
                 <div className="flex flex-col space-y-1 w-full">
                     <label htmlFor="a">Jenis Kelamin</label>
-                    <select 
-                        name="jenisKelamin" 
+                    <select
+                        name="jenisKelamin"
                         id="jenisKelamin"
-                        className='text-gray-900 rounded-md px-2 py-2'
+                        className="text-gray-900 rounded-md px-2 py-2"
                         onChange={(e) =>
                             setRphInfo({
                                 ...rphInfo,
@@ -62,13 +65,17 @@ const RPHPageContainer: FunctionComponent<RPHPageContainerProps> = ({}) => {
                             })
                         }
                     >
-                        <option selected disabled>Choose an Option</option>
+                        <option selected disabled>
+                            Choose an Option
+                        </option>
                         <option value="Jantan">Jantan</option>
                         <option value="Betina">Betina</option>
                     </select>
                 </div>
                 <div className="flex flex-col space-y-1 w-full">
-                    <label htmlFor="tanggalPemotongan">Tanggal Pemotongan</label>
+                    <label htmlFor="tanggalPemotongan">
+                        Tanggal Pemotongan
+                    </label>
                     <input
                         type="date"
                         name="tanggalPemotongan"
@@ -109,7 +116,7 @@ const RPHPageContainer: FunctionComponent<RPHPageContainerProps> = ({}) => {
                         }
                     />
                 </div>
-                <button 
+                <button
                     onClick={handleInput}
                     className="bg-gray-900 hover:bg-gray-800 active:scale-90 transition ease-in-out w-full p-2.5 rounded-xl"
                 >
@@ -152,10 +159,10 @@ const RPHPageContainer: FunctionComponent<RPHPageContainerProps> = ({}) => {
                 pauseOnFocusLoss
                 draggable
                 pauseOnHover
-                style={{width: "350px"}}
+                style={{ width: '350px' }}
             />
         </div>
-   )
+    );
 };
 
 export default RPHPageContainer;
--- pages/RPHPage/index.tsx
+++ pages/RPHPage/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 import RPHPageContainer from '../../modules/RPHPage/RPHPageContainer';
+import Navigation from '../../uikit/Navigation';
 
-const RPHPage:NextPage = () => {
-  return (
-    <>
-      <RPHPageContainer/>
-    </>
-  );
-}
- 
+const RPHPage: NextPage = () => {
+    return (
+        <>
+            <Navigation />
+            <RPHPageContainer />
+        </>
+    );
+};
+
 export default RPHPage;
