a1f8db61553398fc2b21e0e061e9da2f6dd4e647
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-06-07 16:04:43 +0700

    fix: add metadata for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import Navigation from '../uikit/Navigation';
+import PageMetadata from '../uikit/PageMetadata';
 import HomePageContainer from '../modules/HomePage/HomePageContainer';
 
 const Home: NextPage = () => {
     return (
         <div>
             <Navigation />
+            <PageMetadata page="Home" />
             <HomePageContainer />
         </div>
     );
