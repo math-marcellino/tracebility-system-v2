53ea4b5f99522e485f68b986005708ae1178c58f
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-06-09 13:31:23 +0700

    fix: add responsiveness

--- modules/TracePage/TracePageContainer.tsx
+++ modules/TracePage/TracePageContainer.tsx
@@ -15,21 +15,23 @@ const TracePageContainer: FunctionComponent<TracePageContainerProps> = ({}) => {
     const [RPHData, setRPHData] = useState<Result | undefined>();
     console.log(hotelData);
     return (
-        <div className="flex flex-col h-screen items-center justify-center gap-16">
-            <h1 className="font-extrabold text-5xl">Trace</h1>
+        <div className="flex flex-col 2xl:h-screen items-center justify-center gap-16">
+            <h1 className="font-extrabold text-5xl 2xl:pt-0 pt-16">Trace</h1>
             {hotelData?.[0] === '' ? (
                 <div className="bg-gray-800 rounded-xl py-5 px-16">
                     <h1 className="text-3xl font-bold">Data not found!</h1>
                 </div>
             ) : (
-                <div className="flex flex-row items-center justify-center gap-x-8">
+                <div className="flex 2xl:flex-row flex-col items-center justify-center gap-8 2xl:px-12 p-4">
                     <HotelInfoCard setHotelData={setHotelData} />
-                    <p className="text-6xl">&rarr;</p>
+                    <p className="text-6xl hidden 2xl:flex">&rarr;</p>
+                    <p className="text-6xl 2xl:hidden">&darr;</p>
                     <DistributorInfoCard
                         setDistributorData={setDistributorData}
                         distributorBatchID={hotelData?.[0]}
                     />
-                    <p className="text-6xl">&rarr;</p>
+                    <p className="text-6xl hidden 2xl:flex">&rarr;</p>
+                    <p className="text-6xl 2xl:hidden">&darr;</p>
                     <RPHInfoCard
                         setRPHData={setRPHData}
                         RPHBatchID={distributorData?.[0]}
