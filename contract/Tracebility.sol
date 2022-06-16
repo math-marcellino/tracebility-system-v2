fb78c3cdda47c4440ecbbe36d05022d6aafbac93
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-06-16 15:55:25 +0700

    fix: revisi smart contract

--- contract/Tracebility.sol
+++ contract/Tracebility.sol
@@ -15,153 +15,198 @@ contract TraceabilityV2{
     require(msg.sender == owner, "You are not the owner!");
     _;
   }
-  
-  // Phases      
-  mapping(string => RPH) public RPHBatch;
-  mapping(string => Distributor) public DistributorBatch;
-  mapping(string => Hotel) public HotelBatch;
 
-  enum StatusPengiriman {OTW, SAMPAI}
+  // Data Structures
+  struct User{
+    string nama;
+    string sertifikatHalal;
+    string role;
+  }
 
-  struct RPH{
+  struct Pemotongan{
+    string ID_RPH;
     string jenis_kelamin;
     string tanggal_pemotongan; 
-    string status_pemotongan;
-    string verifier;
-    string sertifHalal;
+    string status_kehalalan;
   }
 
-  event RPHTrace(
-    string RPHBatchID,
-    string jenis_kelamin,
-    string tanggal_pemotongan,
-    string status_pemotongan,
-    string verifier,
-    string sertifHalal,
-    uint256 time
-  );
+  struct ProdukRPH{
+    string ID_Pemotongan;
+    string nama;
+    string status_kehalalan;
+  }
 
-  struct Distributor{
-    string RPHBatchID;
+  struct ProdukDistributor{
+    string ID_Distributor;
+    string nama;
     uint256 durasi_penyimpanan;
     string cara_penyimpanan;
-    string status_penyimpanan;
-    string verifier;
-    string sertifHalal;
+    string status_kehalalan;
   }
 
-  event DistributorTrace(
-    string DistributorBatchID,
-    string RPHBatchID,
-    uint256 durasi_penyimpanan,
-    string cara_penyimpanan,
-    string status_penyimpanan,
-    string verifier,
-    string sertifHalal,
-    uint256 time
-  );
-
-  struct Hotel{
-    string DistributorBatchID;
+  struct Makanan{
+    string ID_Hotel;
+    string nama;
     string tanggal_pengolahan;
     string cara_pengolahan;
-    string verifier;
-    string sertifHalal;
+    string status_kehalalan;
   }
 
-  event HotelTrace(
-    string HotelBatchID,
-    string DistributorBatchID,
-    string tanggal_pengolahan,
-    string cara_pengolahan,
-    string verifier,
-    string sertifHalal,
-    uint256 time
+  // Mappings 
+  mapping(string => User) public Users;
+  mapping(string => Pemotongan) public PemotonganBatch;
+  mapping(string => ProdukRPH) public ProdukRPHBatch;
+  mapping(string => ProdukDistributor) public ProdukDistributorBatch;
+  mapping(string => Makanan) public MakananBatch;
+
+  // Events to trace data
+  event TracePemotongan(
+    string ID_Pemotongan,
+    string ID_RPH,
+    string jenis_kelamin,
+    string tanggal_pemotongan,
+    string status_kehalalan,
+    uint256 date
   );
 
-  uint indexRPH;
-  uint indexDistributor;
-  uint indexHotel;
+  event TraceProdukRPH(
+    string ID_ProdukRPH,
+    string ID_Pemotongan,
+    string nama,
+    string status_kehalalan,
+    uint256 date
+  );
+  
+  event TraceProdukDistributor(
+    string ID_ProdukDistributor,
+    string ID_Distributor,
+    string nama,
+    uint256 durasi_penyimpanan,
+    string cara_penyimpanan,
+    string status_kehalalan,
+    uint256 date
+  );
+
+  event TraceMakanan(
+    string ID_Makanan,
+    string ID_Hotel,
+    string nama,
+    string tanggal_pengolahan,
+    string cara_pengolahan,
+    string status_kehalalan,
+    uint256 date
+  );
+
+  // Index to create new ID
+  uint index_pemotongan;
+  uint index_produkRPH;
+  uint index_produkDistributor;
+  uint index_makanan;
+
+  // Functions
+  function register(
+    string memory _role,
+    string memory _nama,
+    string memory _username,
+    string memory _sertifikatHalal
+  ) public {
+    Users[_username].nama = _nama;
+    Users[_username].role = _role;
+    Users[_username].sertifikatHalal = _sertifikatHalal;
+  }
 
   function setDataPemotongan(
-    string memory jenisKelamin,
-    string memory tanggalPemotongan,
-    string memory statusPemotongan,
-    string memory sertifHalal,
-    string memory verifier
+    string memory _ID_RPH,
+    string memory _jenisKelamin,
+    string memory _tanggalPemotongan,
+    string memory _statusKehalalan
   ) public onlyOwner {
-      indexRPH++;
-      string memory ID = string.concat("RPH", Strings.toString(indexRPH));
-      RPHBatch[ID].jenis_kelamin = jenisKelamin;
-      RPHBatch[ID].tanggal_pemotongan = tanggalPemotongan;
-      RPHBatch[ID].status_pemotongan = statusPemotongan;
-      RPHBatch[ID].sertifHalal = sertifHalal;
-      RPHBatch[ID].verifier = verifier;
+    index_pemotongan++;
+    string memory ID = string.concat("PMTG", Strings.toString(index_pemotongan));
+    PemotonganBatch[ID].ID_RPH = _ID_RPH;
+    PemotonganBatch[ID].jenis_kelamin = _jenisKelamin;
+    PemotonganBatch[ID].tanggal_pemotongan = _tanggalPemotongan;
+    PemotonganBatch[ID].status_kehalalan = _statusKehalalan;
 
-      emit RPHTrace(
-        ID,
-        RPHBatch[ID].jenis_kelamin,
-        RPHBatch[ID].tanggal_pemotongan,
-        RPHBatch[ID].status_pemotongan,
-        RPHBatch[ID].verifier,
-        RPHBatch[ID].sertifHalal,
-        block.timestamp
-      );
-  }
-
-  function setDataPenyimpanan(
-    string memory RPHBatchID,
-    uint256 durasiPenyimpanan,
-    string memory caraPenyimpanan,
-    string memory statusPenyimpanan,
-    string memory sertifHalal,
-    string memory verifier
-  ) public onlyOwner {
-      indexDistributor++;
-      string memory ID = string.concat("DISTRIBUTOR", Strings.toString(indexDistributor));
-      DistributorBatch[ID].RPHBatchID = RPHBatchID;
-      DistributorBatch[ID].durasi_penyimpanan = durasiPenyimpanan;
-      DistributorBatch[ID].cara_penyimpanan = caraPenyimpanan;
-      DistributorBatch[ID].status_penyimpanan = statusPenyimpanan;
-      DistributorBatch[ID].sertifHalal = sertifHalal;
-      DistributorBatch[ID].verifier = verifier;
-
-      emit DistributorTrace(
-        ID,
-        DistributorBatch[ID].RPHBatchID = RPHBatchID,
-        DistributorBatch[ID].durasi_penyimpanan,
-        DistributorBatch[ID].cara_penyimpanan,
-        DistributorBatch[ID].status_penyimpanan,
-        DistributorBatch[ID].verifier,
-        DistributorBatch[ID].sertifHalal,
-        block.timestamp
-      );
-  }
-
-  function setDataPengolahan(
-    string memory DistributorBatchID,
-    string memory tanggalPengolahan,
-    string memory caraPengolahan,
-    string memory sertifHalal,
-    string memory verifier
-  ) public onlyOwner {
-    indexHotel++;
-    string memory ID = string.concat("HOTEL", Strings.toString(indexHotel));
-    HotelBatch[ID].DistributorBatchID = DistributorBatchID;
-    HotelBatch[ID].tanggal_pengolahan = tanggalPengolahan;
-    HotelBatch[ID].cara_pengolahan = caraPengolahan;
-    HotelBatch[ID].sertifHalal = sertifHalal;
-    HotelBatch[ID].verifier = verifier;
-
-    emit HotelTrace(
+    emit TracePemotongan(
       ID,
-      HotelBatch[ID].DistributorBatchID = DistributorBatchID,
-      HotelBatch[ID].tanggal_pengolahan,
-      HotelBatch[ID].cara_pengolahan,
-      HotelBatch[ID].verifier,
-      HotelBatch[ID].sertifHalal,
+      _ID_RPH,
+      _jenisKelamin,
+      _tanggalPemotongan,
+      _statusKehalalan,
+      block.timestamp
+    );
+  }
+
+  function setDataProdukRPH(
+    string memory _ID_Pemotongan,
+    string memory _nama,
+    string memory _statusKehalalan
+  ) public onlyOwner {
+    index_produkRPH++;
+    string memory ID = string.concat("PRPH", Strings.toString(index_produkRPH));
+    ProdukRPHBatch[ID].ID_Pemotongan = _ID_Pemotongan;
+    ProdukRPHBatch[ID].nama = _nama;
+    ProdukRPHBatch[ID].status_kehalalan = _statusKehalalan;
+
+    emit TraceProdukRPH(
+      ID,
+      _ID_Pemotongan,
+      _nama,
+      _statusKehalalan,
+      block.timestamp
+    );
+  }
+
+  function setDataProdukDistributor(
+    string memory _ID_Distributor,
+    string memory _nama,
+    uint256 _durasiPenyimpanan,
+    string memory _caraPenyimpanan,
+    string memory _statusKehalalan
+  ) public onlyOwner {
+    index_produkDistributor++;
+    string memory ID = string.concat("PDIS", Strings.toString(index_produkDistributor));
+    ProdukDistributorBatch[ID].ID_Distributor = _ID_Distributor;
+    ProdukDistributorBatch[ID].nama = _nama;
+    ProdukDistributorBatch[ID].durasi_penyimpanan = _durasiPenyimpanan;
+    ProdukDistributorBatch[ID].cara_penyimpanan = _caraPenyimpanan;
+    ProdukDistributorBatch[ID].status_kehalalan = _statusKehalalan;
+
+    emit TraceProdukDistributor(
+      ID,
+      _ID_Distributor,
+      _nama,
+      _durasiPenyimpanan,
+      _caraPenyimpanan,
+      _statusKehalalan,
+      block.timestamp
+    );
+  }
+
+  function setDataMakanan(
+    string memory _ID_Hotel,
+    string memory _nama,
+    string memory _tanggalPengolahan,
+    string memory _caraPengolahan,
+    string memory _statusKehalalan
+  ) public onlyOwner {
+    index_makanan++;
+    string memory ID = string.concat("PMKN", Strings.toString(index_makanan));
+    MakananBatch[ID].ID_Hotel = _ID_Hotel;
+    MakananBatch[ID].nama = _nama;
+    MakananBatch[ID].tanggal_pengolahan = _tanggalPengolahan;
+    MakananBatch[ID].cara_pengolahan = _caraPengolahan;
+    MakananBatch[ID].status_kehalalan = _statusKehalalan;
+
+    emit TraceMakanan(
+      ID,
+      _ID_Hotel,
+      _nama,
+      _tanggalPengolahan,
+      _caraPengolahan,
+      _statusKehalalan,
       block.timestamp
     );
   }
 }
-
